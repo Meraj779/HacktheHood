dfbb2b8041c3945c16d8c75582d41174733c595b
Meraj779 <Alimimeraj@gmail.com>
2021-11-30 16:47:18 -0800

    Finished

---
+++ error.handling.py
@@ -0,0 +1,13 @@
+"""
+We're writing a program that adds two numbers together. 
+Test this and solve any errors you find if you can. 
+Solving one problem might turn up a new error...
+Be sure to keep a note of whatever errors you find!
+"""
+
+
+print("Welcome to the addition helper")
+number1 = input("Please enter the first number:")
+number2 = input("Please enter the second number:")
+
+print("The result is: ", number1 + number2)
